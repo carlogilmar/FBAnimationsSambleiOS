c521881c68bc9609017ba66fa23ef3ccf468380a
carlogilmar <carlogilmar12@gmail.com>
2018-06-24 01:52:57 -0500

    Drawing a simple line

--- FBStreamAnimation/ViewController.swift
+++ FBStreamAnimation/ViewController.swift
@@ -12,14 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         let curvedView = CurvedView(frame: view.frame)
         curvedView.backgroundColor = .yellow
         view.addSubview(curvedView)
-        
     }
-
-    
     
 }
 
@@ -30,8 +26,12 @@ class CurvedView: UIView {
         let path = UIBezierPath()
         let startPoint = CGPoint(x: 0, y:200)
         path.move(to: startPoint)
-        let endPoint = CGPoint(x: 200, y: 200)
-        path.addLine(to: endPoint)
+        let endPoint = CGPoint(x: 400, y: 200)
+        //path.addLine(to: endPoint)
+        let controlPoint1 = CGPoint(x: 100, y: 100)
+        let controlPoint2 = CGPoint(x: 200, y: 300)
+        path.addCurve(to: endPoint, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+        path.lineWidth = 3
         path.stroke()
     }
     
